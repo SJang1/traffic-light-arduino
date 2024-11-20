ea212b157e9e08007882758ab9d60abefa963a30
SJang1 <git@sjang.dev>
2024-11-20 15:17:58 +0900

    add 4dig 7seg anode

--- 2560/2560.ino
+++ 2560/2560.ino
@@ -4,11 +4,38 @@
 #define yellow_light_pin 36
 #define red_light_pin 35
 
+const int segmentPins[] = {2, 3, 4, 5, 6, 7, 8, 9}; // A, B, C, D, E, F, G, DP
+const int digitPins[] = {10, 11, 12, 13}; // Digit control pins (common anodes)
+
+const byte digitToSegment[] = {
+  0b00111111, // 0
+  0b00000110, // 1
+  0b01011011, // 2
+  0b01001111, // 3
+  0b01100110, // 4
+  0b01101101, // 5
+  0b01111101, // 6
+  0b00000111, // 7
+  0b01111111, // 8
+  0b01101111  // 9
+};
+
 String current_light = "red";
 
 // defines variables
 long duration;
 int distance;
+
+void DISP_SETUP() {
+  // Set segment pins as OUTPUT
+  for (int i = 0; i < 8; i++) {
+    pinMode(segmentPins[i], OUTPUT);
+  }
+  // Set digit pins as OUTPUT
+  for (int i = 0; i < 4; i++) {
+    pinMode(digitPins[i], OUTPUT);
+  }
+}
 
 void HC_SR04_SETUP() {
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
@@ -26,6 +53,33 @@ void setup() {
   Serial1.begin(9600);     // Serial to ESP8266
   HC_SR04_SETUP();
   light_setup();
+}
+
+void DISP_PRINT(int number) {
+  // Extract individual digits
+  int digits[4] = {0, 0, 0, 0};
+  digits[3] = number % 10;
+  digits[2] = (number / 10) % 10;
+  digits[1] = (number / 100) % 10;
+  digits[0] = (number / 1000) % 10;
+
+  // Multiplex through each digit
+  for (int i = 0; i < 4; i++) {
+    // Activate the current digit (set LOW for common anode)
+    digitalWrite(digitPins[i], LOW);
+
+    // Write the segments
+    for (int j = 0; j < 8; j++) {
+      bool isOn = digitToSegment[digits[i]] & (1 << j);
+      digitalWrite(segmentPins[j], isOn ? HIGH : LOW);
+    }
+
+    // Wait for a short time to allow the digit to appear
+    delay(5);
+
+    // Deactivate the current digit (set HIGH for common anode)
+    digitalWrite(digitPins[i], HIGH);
+  }
 }
 
 int HC_SR04_LOOP() {
@@ -49,24 +103,28 @@ void print_green_light() {
   digitalWrite(green_light_pin, HIGH);
   digitalWrite(yellow_light_pin, LOW);
   digitalWrite(red_light_pin, LOW);
+  delay(5);
 }
 
 void print_yellow_light() {
   digitalWrite(green_light_pin, LOW);
   digitalWrite(yellow_light_pin, HIGH);
   digitalWrite(red_light_pin, LOW);
+  delay(5);
 }
 
 void print_red_light() {
   digitalWrite(green_light_pin, LOW);
   digitalWrite(yellow_light_pin, LOW);
   digitalWrite(red_light_pin, HIGH);
+  delay(5);
 }
 
 void loop() {
   int distance_loop = HC_SR04_LOOP();
+  DISP_PRINT(distance_loop);
 
-  if (distance_loop <= 8)
+  if (distance_loop <= 8) {
     print_green_light();
     current_light = "green";
     Serial1.println("green:" + String(distance_loop));
@@ -78,6 +136,7 @@ void loop() {
       Serial1.println("yellow:" + String(distance_loop));
       delay(2000);
       distance_loop = HC_SR04_LOOP(); // revalidate because of delay time
+      DISP_PRINT(distance_loop);
     }
     print_red_light();
     current_light = "red";
