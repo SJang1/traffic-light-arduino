93c34c9dec79ed0b27002847e9c8c88cd7ea8469
siliconsjang <siliconsjang@siliconsjangui-MacBookPro.local>
2024-11-20 19:32:07 +0900

    2 traffics

--- 2560/2560.ino
+++ 2560/2560.ino
@@ -20,26 +20,25 @@ const byte digitToSegment[] = {
   0b01101111  // 9
 };
 
-String current_light = "red";
+String tram_light = "red";
+String car_light_status = "green";
 
 // defines variables
 long duration;
 int distance;
 
 void DISP_SETUP() {
-  // Set segment pins as OUTPUT
   for (int i = 0; i < 8; i++) {
     pinMode(segmentPins[i], OUTPUT);
   }
-  // Set digit pins as OUTPUT
   for (int i = 0; i < 4; i++) {
     pinMode(digitPins[i], OUTPUT);
   }
 }
 
 void HC_SR04_SETUP() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT);  // Sets the echoPin as an Input
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
 }
 
 void light_setup() {
@@ -49,36 +48,30 @@ void light_setup() {
 }
 
 void setup() {
-  Serial.begin(9600);      // Serial monitor
-  Serial1.begin(9600);     // Serial to ESP8266
+  Serial.begin(9600);
+  Serial1.begin(9600);
   HC_SR04_SETUP();
   light_setup();
   DISP_SETUP();
 }
 
 void DISP_PRINT(int number) {
-  // Extract individual digits
   int digits[4] = {0, 0, 0, 0};
   digits[3] = number % 10;
   digits[2] = (number / 10) % 10;
   digits[1] = (number / 100) % 10;
   digits[0] = (number / 1000) % 10;
 
-  // Multiplex through each digit
   for (int i = 0; i < 4; i++) {
-    // Activate the current digit (set LOW for common anode)
     digitalWrite(digitPins[i], LOW);
 
-    // Write the segments
     for (int j = 0; j < 8; j++) {
       bool isOn = digitToSegment[digits[i]] & (1 << j);
       digitalWrite(segmentPins[j], isOn ? HIGH : LOW);
     }
 
-    // Wait for a short time to allow the digit to appear
     delay(5);
 
-    // Deactivate the current digit (set HIGH for common anode)
     digitalWrite(digitPins[i], HIGH);
   }
 }
@@ -90,10 +83,7 @@ int HC_SR04_LOOP() {
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
 
-  // Reads the echoPin, returns the sound wave travel time in microseconds
   duration = pulseIn(echoPin, HIGH);
-
-  // Calculating the distance
   distance = duration * 0.034 / 2;
   Serial.print("Distance: ");
   Serial.println(distance);
@@ -104,45 +94,49 @@ void print_green_light() {
   digitalWrite(green_light_pin, HIGH);
   digitalWrite(yellow_light_pin, LOW);
   digitalWrite(red_light_pin, LOW);
-  delay(5);
 }
 
 void print_yellow_light() {
   digitalWrite(green_light_pin, LOW);
   digitalWrite(yellow_light_pin, HIGH);
   digitalWrite(red_light_pin, LOW);
-  delay(5);
 }
 
 void print_red_light() {
   digitalWrite(green_light_pin, LOW);
   digitalWrite(yellow_light_pin, LOW);
   digitalWrite(red_light_pin, HIGH);
-  delay(5);
 }
 
 void loop() {
   int distance_loop = HC_SR04_LOOP();
   DISP_PRINT(distance_loop);
 
-  if (distance_loop >= 10) {
-    print_green_light();
-    current_light = "green";
-    Serial1.println("green:" + String(distance_loop));
+  if (distance_loop <= 10 && tram_light != "green") {
+    // Car light to yellow, then red
+    print_yellow_light();
+    car_light_status = "yellow";
+    Serial1.println("tram:" + String(distance_loop) + ":red car:" + car_light_status);
     delay(2000);
-  } else if (distance_loop < 10) {
-    if (current_light == "green") {
-      print_yellow_light();
-      current_light = "yellow";
-      Serial1.println("yellow:" + String(distance_loop));
-      delay(2000);
-      distance_loop = HC_SR04_LOOP(); // revalidate because of delay time
-      DISP_PRINT(distance_loop);
-    }
-    print_red_light();
-    current_light = "red";
-    Serial1.println("red:" + String(distance_loop));
-    delay(2000);
-  }
 
+    print_red_light();
+    car_light_status = "red";
+    tram_light = "green";
+    Serial1.println("tram:" + String(distance_loop) + ":" + tram_light + " car:-1:" + car_light_status);
+    delay(2000);
+  } else if (distance_loop > 10 && tram_light == "green") {
+    // Tram light to red, car light to green
+    print_red_light();
+    tram_light = "red";
+    delay(2000);
+
+    print_green_light();
+    car_light_status = "green";
+    Serial1.println("tram:" + String(distance_loop) + ":" + tram_light + " car:-1" + car_light_status);
+    delay(2000);
+  } else {
+    // Maintain current states
+    Serial1.println("tram:" + String(distance_loop) + ":" + tram_light + " car:-1" + car_light_status);
+    delay(1000);
+  }
 }
--- esp/esp.ino
+++ esp/esp.ino
@@ -5,16 +5,18 @@
 const char* ssid = "SJ iPhone 15 PM";
 const char* password = "1234567890";
 
-
-//String serverName = "https://traffic.sjang.dev/api/update"; // Production (Turn on Argo before Showing users)
+// String serverName = "https://traffic.sjang.dev/api/update"; // Production (Turn on Argo before Showing users)
 String serverName = "https://traffic.sjy-dae.zone/api/update"; // dev
-
 
 int flashbutton;
 int currentState = 0;
 
+String data1 = ""; // Holds data for ID 1
+String data2 = ""; // Holds data for ID 2
+
 void setup() {
   Serial.begin(9600);  // For communication with Mega
+  Serial1.begin(9600); // For communication with another device
   WiFi.begin(ssid, password);
 
   Serial.println("Connecting to WiFi...");
@@ -29,43 +31,70 @@ void setup() {
 
 void loop() {
   flashbutton = digitalRead(0);
-  // Using Falsh Button, Change color
-  if (flashbutton== 0) {
-    String states[] = {"green:1000", "yellow:500", "red:800"};
-    String datasend = states[currentState];
-    sendToServer(datasend);
+
+if (flashbutton == 0) {
+  static unsigned long lastPressTime = 0;
+  unsigned long currentPressTime = millis();
+
+  // Debounce logic to prevent rapid toggling
+  if (currentPressTime - lastPressTime > 500) { // 500ms debounce delay
+    // Define the states as combined strings
+    String states[] = {"1:1000:green 2:800:red", "1:500:yellow 2:800:red", "1:800:red 2:1000:green"};
+
+    // Get the current state string
+    String state = states[currentState];
+
+    // Split the string into data1 and data2
+    data1 = parseData(state, 1); // Parse for ID 1
+    data2 = parseData(state, 2); // Parse for ID 2
+
+    // Update the current state for the next button press
     currentState = (currentState + 1) % 3;
-    if (currentState >= 3) {
-      currentState = 0; // Block to be overflow
-    }
+
+    // Send the combined payload for ID 1 and ID 2
+    sendToServer();
+
+    // Update the last press time
+    lastPressTime = currentPressTime;
   }
-  // Use Serial TX/RX
-  if (Serial.available()) {
-    String data = Serial.readStringUntil('\n'); // Read data from Mega
+}
+
+
+  // Use Serial1 for receiving data and prepare data for ID 2
+  if (Serial1.available()) {
+    String data = Serial1.readStringUntil('\n'); // Read data from Serial1
     data.trim();
     if (data.length() > 0) {
-      sendToServer(data);
+      data2 = formatData(data, 2); // Format data for ID 2
+      sendToServer(); // Send the combined payload for ID 1 and ID 2
     }
   }
 }
 
-void sendToServer(String message) {
+String formatData(String message, int id) {
+  String color = message.substring(0, message.indexOf(':'));
+  int distance = message.substring(message.indexOf(':') + 1).toInt();
+
+  return String(id) + ":{\"status\": \"" + color + "\", \"distance_cm\": " + String(distance) + "}";
+}
+
+void sendToServer() {
   if (WiFi.status() == WL_CONNECTED) {
-    //WiFiClient client;
     HTTPClient http;
-
-    // HTTPS
     WiFiClientSecure client;
-    client.setInsecure();
-
-    String color = message.substring(0, message.indexOf(':'));
-    int distance = message.substring(message.indexOf(':') + 1).toInt();
+    client.setInsecure(); // Use insecure connection for HTTPS
 
     http.begin(client, serverName);
     http.addHeader("Content-Type", "application/json");
 
-    String jsonPayload = "{\"status\": \"" + color + "\", \"distance_cm\": " + String(distance) + "}";
-    Serial.println(jsonPayload);
+    // Construct the payload with data1 and data2
+    String jsonPayload = "{";
+    if (data1.length() > 0) jsonPayload += data1; // Add data for ID 1 if available
+    if (data1.length() > 0 && data2.length() > 0) jsonPayload += ", "; // Add comma if both data exist
+    if (data2.length() > 0) jsonPayload += data2; // Add data for ID 2 if available
+    jsonPayload += "}";
+
+    Serial.println("Sending JSON Payload: " + jsonPayload);
     int httpResponseCode = http.POST(jsonPayload);
 
     if (httpResponseCode > 0) {
