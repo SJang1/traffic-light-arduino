776db1362c9b0d440b6113dbc5620e763ffd37f6
siliconsjang <siliconsjang@siliconsjangui-MacBookPro.local>
2024-11-20 20:11:59 +0900

    fix

--- esp/esp.ino
+++ esp/esp.ino
@@ -14,21 +14,18 @@ int currentState = 0;
 String data1 = ""; // Holds data for ID 1
 String data2 = ""; // Holds data for ID 2
 
-
 String parseData(String state, int id) {
   String prefix = String(id) + ":";
   int startIndex = state.indexOf(prefix);
   if (startIndex != -1) {
     int endIndex = state.indexOf(" ", startIndex); // Find the end of this ID's data
-    if (endIndex == -1) { 
+    if (endIndex == -1) {
       endIndex = state.length(); // If no space, take the rest of the string
     }
     return state.substring(startIndex, endIndex); // Extract the substring for the specific ID
   }
   return ""; // Return an empty string if the ID is not found
 }
-
-
 
 void setup() {
   Serial.begin(9600);  // For communication with Mega
@@ -47,33 +44,32 @@ void setup() {
 void loop() {
   flashbutton = digitalRead(0);
 
-if (flashbutton == 0) {
-  static unsigned long lastPressTime = 0;
-  unsigned long currentPressTime = millis();
+  if (flashbutton == 0) {
+    static unsigned long lastPressTime = 0;
+    unsigned long currentPressTime = millis();
 
-  // Debounce logic to prevent rapid toggling
-  if (currentPressTime - lastPressTime > 500) { // 500ms debounce delay
-    // Define the states as combined strings
-    String states[] = {"1:1000:green 2:-1:red", "1:500:yellow 2:-1:red", "1:800:red 2:-1:green"};
+    // Debounce logic to prevent rapid toggling
+    if (currentPressTime - lastPressTime > 500) { // 500ms debounce delay
+      // Define the states as combined strings
+      String states[] = {"1:1000:green 2:-1:red", "1:500:yellow 2:-1:red", "1:800:red 2:-1:green"};
 
-    // Get the current state string
-    String state = states[currentState];
+      // Get the current state string
+      String state = states[currentState];
 
-    // Split the string into data1 and data2
-    data1 = parseData(state, 1); // Parse for ID 1
-    data2 = parseData(state, 2); // Parse for ID 2
+      // Split the string into data1 and data2
+      data1 = parseData(state, 1); // Parse for ID 1
+      data2 = parseData(state, 2); // Parse for ID 2
 
-    // Update the current state for the next button press
-    currentState = (currentState + 1) % 3;
+      // Update the current state for the next button press
+      currentState = (currentState + 1) % 3;
 
-    // Send the combined payload for ID 1 and ID 2
-    sendToServer();
+      // Send the combined payload for ID 1 and ID 2
+      sendToServer();
 
-    // Update the last press time
-    lastPressTime = currentPressTime;
+      // Update the last press time
+      lastPressTime = currentPressTime;
+    }
   }
-}
-
 
   // Use Serial1 for receiving data and prepare data for ID 2
   if (Serial1.available()) {
@@ -89,10 +85,17 @@ if (flashbutton == 0) {
 }
 
 String formatData(String message, int id) {
-  String color = message.substring(0, message.indexOf(':'));
-  int distance = message.substring(message.indexOf(':') + 1).toInt();
+  int firstColonIndex = message.indexOf(":");
+  int lastColonIndex = message.lastIndexOf(":");
+  
+  if (firstColonIndex == -1 || lastColonIndex == -1 || firstColonIndex == lastColonIndex) {
+    return ""; // Return an empty string if the format is incorrect
+  }
 
-  return String(id) + ":{\"status\": \"" + color + "\", \"distance_cm\": " + String(distance) + "}";
+  String distance = message.substring(firstColonIndex + 1, lastColonIndex);
+  String color = message.substring(lastColonIndex + 1);
+
+  return String(id) + ":{\"status\": \"" + color + "\", \"distance_cm\": " + distance + "}";
 }
 
 void sendToServer() {
@@ -106,9 +109,18 @@ void sendToServer() {
 
     // Construct the payload with data1 and data2
     String jsonPayload = "{";
-    if (data1.length() > 0) jsonPayload += data1; // Add data for ID 1 if available
-    if (data1.length() > 0 && data2.length() > 0) jsonPayload += ", "; // Add comma if both data exist
-    if (data2.length() > 0) jsonPayload += data2; // Add data for ID 2 if available
+    bool firstData = true;
+
+    if (data1.length() > 0) {
+      jsonPayload += formatData(data1, 1);
+      firstData = false;
+    }
+
+    if (data2.length() > 0) {
+      if (!firstData) jsonPayload += ", ";
+      jsonPayload += formatData(data2, 2);
+    }
+
     jsonPayload += "}";
 
     Serial.println("Sending JSON Payload: " + jsonPayload);
