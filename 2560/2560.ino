19381a9d01fcc2bfd3bd44625833290d079fbf62
SJang1 <git@sjang.dev>
2024-12-06 14:16:51 +0900

    Update 2560.ino

--- 2560/2560.ino
+++ 2560/2560.ino
@@ -19,6 +19,7 @@ const int redgreenchangecm = 15;
 const int segmentPins[] = {2, 3, 4, 5, 6, 7, 8, 9}; // A, B, C, D, E, F, G, DP
 const int digitPins[] = {10, 11, 12, 13}; // Digit control pins (common anodes)
 
+// 4digit 7segment display 숫자
 const byte digitToSegment[] = {
   0b00111111, // 0
   0b00000110, // 1
@@ -35,7 +36,6 @@ const byte digitToSegment[] = {
 String tram_light = "red";
 String car_light = "green";
 
-// defines variables
 long duration;
 int distance;
 int distance_loop = 0;
@@ -49,6 +49,7 @@ const unsigned long displayInterval = 5; // Time interval for refreshing display
 bool displayOn = true;
 int displayNumber = 0;
 
+// 4digit 7segment display 초기화
 void DISP_SETUP() {
   for (int i = 0; i < 8; i++) {
     pinMode(segmentPins[i], OUTPUT);
@@ -58,33 +59,37 @@ void DISP_SETUP() {
   }
 }
 
+// 초음파 센서 초기화
 void HC_SR04_SETUP() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
 
+// LED 초기화
 void light_setup() {
   pinMode(green_light_pin, OUTPUT);
   pinMode(yellow_light_pin, OUTPUT);
   pinMode(red_light_pin, OUTPUT);
 }
 
+// 초기설정
 void setup() {
   Serial.begin(9600);
   Serial1.begin(9600);
   HC_SR04_SETUP();
   light_setup();
   DISP_SETUP();
-
-      greenInProgress = true;
-    yellowInProgress = false;
-    redInProgress = false;
-    tram_light = "red";
-    car_light = "green";
-    print_green_light();
+  greenInProgress = true;
+  yellowInProgress = false;
+  redInProgress = false;
+  tram_light = "red";
+  car_light = "green";
+  print_green_light();
 
 }
 
+// 4digit 7segment display 표시. 
+// 각 digit별로 아주 짧게 표시를 하여 사람의 눈에는 모두 동시에 불 들어온 것 처럼 보이게 됩니다.
 void DISP_PRINT(int number) {
   int digits[4] = {0, 0, 0, 0};
   digits[3] = number % 10;
@@ -110,6 +115,7 @@ void DISP_PRINT(int number) {
   }
 }
 
+// 초음파 센서로 거리 측정해서 숫자형식으로 반환
 int HC_SR04_LOOP() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
@@ -124,24 +130,28 @@ int HC_SR04_LOOP() {
   return distance;
 }
 
+// 초록불 LED 출력
 void print_green_light() {
   digitalWrite(green_light_pin, HIGH);
   digitalWrite(yellow_light_pin, LOW);
   digitalWrite(red_light_pin, LOW);
 }
 
+// 노란불 LED 출력
 void print_yellow_light() {
   digitalWrite(green_light_pin, LOW);
   digitalWrite(yellow_light_pin, HIGH);
   digitalWrite(red_light_pin, LOW);
 }
 
+// 빨간불 LED 출력
 void print_red_light() {
   digitalWrite(green_light_pin, LOW);
   digitalWrite(yellow_light_pin, LOW);
   digitalWrite(red_light_pin, HIGH);
 }
 
+// 반복문.
 void loop() {
   unsigned long currentTime = millis(); // Get the current time
 
@@ -153,8 +163,8 @@ void loop() {
   DISP_PRINT(distance_loop);
 
 
+  // 차량불 초록불일때, 거리가 15cm 이내로 들어오면 노란불로
   if (greenInProgress && distance_loop <= 15) {
-    
     greenInProgress = false;
     yellowInProgress = true;
     redInProgress = false;
@@ -163,6 +173,7 @@ void loop() {
     print_yellow_light();
     yellowStartTime = currentTime;
   }
+  // 노란불 상태에서 3초 이후 차량 빨간불
   if (yellowInProgress &&  currentTime - yellowStartTime >= 3000) {
     greenInProgress = false;
     yellowInProgress = false;
@@ -171,6 +182,7 @@ void loop() {
     car_light = "red";
     print_red_light();
   }
+  // 차량 빨간불 상태에서 거리가 15cm 밖으로 나가면 차량 초록불로
   if (redInProgress && distance_loop > 15) {
     greenInProgress = true;
     yellowInProgress = false;
@@ -180,6 +192,7 @@ void loop() {
     print_green_light();
   }
 
+  // 2초마다 한번씩 인터넷에 전송
   if (currentTime - internet >= 2000) {
   Serial1.println("1:" + String(distance_loop) + ":" + tram_light + " 2:-1:" + car_light);
   internet = currentTime;
